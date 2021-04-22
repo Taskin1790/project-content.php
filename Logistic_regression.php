7cf49cbf4c116fb23097a2d0fa7c78aa03954273
Taskin1790 <82331915+Taskin1790@users.noreply.github.com>
2021-04-22 12:01:18 +0600

    Add files via upload

---
+++ Logistic_regression.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="\E-Learning-Website\home.css" rel="stylesheet" ">
+    <link href="deep_Learning.css" rel="stylesheet" ">
+   <title>Machine Learning</title>
+</head>
+<body>
+    <div class=" Main_wrapper">
+    <div class="nav_bar">
+        <div class="logo">
+            <img src="\E-Learning-Website\images\innovation_icon1.png" class="nav_bar_icon">
+            <h5 class="nav_bar_logo_name">AI LEARNERS</h5>
+        </div>
+        <div class="menu_name">
+            <ul class="ul_nav">
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\homepage.php">Home</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\artificial_intellegence\introduction_ai.php">Artificial Intellegence</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="Introduction_ml.php">Machine Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Deep_learning\introduction_dl.php">Deep Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Robotics\introduction_robotic.php">Robotics</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\python\introduction_py.php">Python</a></li>
+                <li class="item signUp"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_up\sign_up.php">Sign Up</a></li>
+                <li class="item signIn"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_in\sign_in.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main_body">
+
+        <div class="left_body">
+            <div class="left_body_wrapper">
+
+                <h6 class="topic_header">Deep Learning Topics</h6>
+                <a href="Introduction_ml.php" class="topic">Introduction</a>
+                <a href="Learning_path_ml.php" class="topic">Learning Path</a>
+                <a href="Data_preprocessing.php" class="topic">Data Preprocessing</a>
+                <a href="Features_scaling.php" class="topic">Features Scaling</a>
+                <a href="Gradient_descent.php" class="topic">Gradient Descent</a>
+                <a href="Linear_regression.php" class="topic">Linear Regression </a>
+                <a href="Logistic_regression.php" class="topic">Logistic Regression </a>
+                <a href="Decision_tree.php" class="topic">Decision Tree </a>
+                <a href="Regression_vs_classification.php" class="topic">Regression vs. classification</a>
+            </div>
+        </div>
+        <div class="middle_body">
+       <h1> Logistic Regression in Machine Learning</h1>
+
+
+<ul> 
+    <li>Logistic regression is one of the most popular Machine Learning algorithms, which comes under the Supervised Learning technique. It is used for predicting the categorical dependent variable using a given set of independent variables.</li>
+    <li>Logistic Regression is much similar to the Linear Regression except that how they are used. Linear Regression is used for solving Regression problems, whereas Logistic regression is used for solving the classification problems.</li>
+    <li>In Logistic regression, instead of fitting a regression line, we fit an "S" shaped logistic function, which predicts two maximum values (0 or 1).</li>
+    <li>The curve from the logistic function indicates the likelihood of something such as whether the cells are cancerous or not, a mouse is obese or not based on its weight, etc</li>
+    <li>Logistic Regression is a significant machine learning algorithm because it has the ability to provide probabilities and classify new data using continuous and discrete datasets.</li>
+
+</ul>
+  
+  <h2>Logistic Function (Sigmoid Function)</h2>
+   <ul>
+     <li>The sigmoid function is a mathematical function used to map the predicted values to probabilities.</li>
+     <li>It maps any real value into another value within a range of 0 and 1.</li>
+     <li>The value of the logistic regression must be between 0 and 1, which cannot go beyond this limit, so it forms a curve like the "S" form. The S-form curve is called the Sigmoid function or the logistic function.</li>
+
+
+
+   <h2>Type of Logistic Regression</h2>
+   <p>On the basis of the categories, Logistic Regression can be classified into three types:</p>
+   <ul> 
+    <li>Binomial: In binomial Logistic regression, there can be only two possible types of the dependent variables, such as 0 or 1, Pass or Fail, etc.</li> 
+    <li>Multinomial: In multinomial Logistic regression, there can be 3 or more possible unordered types of the dependent variable, such as "cat", "dogs", or "sheep"</li>
+    <li>Ordinal: In ordinal Logistic regression, there can be 3 or more possible ordered types of dependent variables, such as "low", "Medium", or "High"..</li>
+    
+</ul>
+
+<br><br>
+
+
+
+
+</div>
+        <div class="right_body">
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer_wrapper">
+            <div class="left_footer">
+                <h5 class="footer_h5">AI Learners</h5>
+                <a href="#"><img src="\E-Learning-Website\images\facebook_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\twitter_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\youtube_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\instagram_icon.png" class="footer_icon"></a>
+
+
+
+
+            </div>
+            <div class="middle_footer middle_footer1">
+                <p class="footer_p">Support</p>
+                <a href="#" class="footer_link">Contact Us</a>
+                <a href="#" class="footer_link">FAQ</a>
+                <a href="#" class="footer_link">Lecture</a>
+
+
+            </div>
+            <div class="right_footer">
+                <p class="footer_p">AI Learners</p>
+                <a href="#" class="footer_link">About Us</a>
+                <a href="#" class="footer_link">Careers</a>
+                <a href="#" class="footer_link">Access</a>
+            </div>
+            <div class="sign_up_footer">
+                <span class="footer_h4">Stay up to date on the latest from AI Learners</span>
+                <input type="email" placeholder="Enter  your email" class="input_email_for_sign_up">
+                <input type="button" value="Sign Up" class="sign_up_btn">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        
+        <small class="footer__copyright">© 2021 Copyright</small>
+    </div>
+    </div>
+    </body>
+
+</html>
