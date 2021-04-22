78726c68667b7564f2aaa4b619e7d73ca198a3de
Taskin1790 <82331915+Taskin1790@users.noreply.github.com>
2021-04-22 12:00:26 +0600

    Add files via upload

---
+++ Learning_path_ml.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="\E-Learning-Website\home.css" rel="stylesheet" ">
+    <link href="deep_Learning.css" rel="stylesheet" ">
+   <title>Machine Learning</title>
+</head>
+<body>
+    <div class=" Main_wrapper">
+    <div class="nav_bar">
+        <div class="logo">
+            <img src="\E-Learning-Website\images\innovation_icon1.png" class="nav_bar_icon">
+            <h5 class="nav_bar_logo_name">AI LEARNERS</h5>
+        </div>
+        <div class="menu_name">
+            <ul class="ul_nav">
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\homepage.php">Home</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\artificial_intellegence\introduction_ai.php">Artificial Intellegence</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="Introduction_ml.php">Machine Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Deep_learning\introduction_dl.php">Deep Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Robotics\introduction_robotic.php">Robotics</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\python\introduction_py.php">Python</a></li>
+                <li class="item signUp"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_up\sign_up.php">Sign Up</a></li>
+                <li class="item signIn"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_in\sign_in.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main_body">
+
+        <div class="left_body">
+            <div class="left_body_wrapper">
+
+                <h6 class="topic_header">Deep Learning Topics</h6>
+                <a href="Introduction_ml.php" class="topic">Introduction</a>
+                <a href="Learning_path_ml.php" class="topic">Learning Path</a>
+                <a href="Data_preprocessing.php" class="topic">Data Preprocessing</a>
+                <a href="Features_scaling.php" class="topic">Features Scaling</a>
+                <a href="Gradient_descent.php" class="topic">Gradient Descent</a>
+                <a href="Linear_regression.php" class="topic">Linear Regression </a>
+                <a href="Logistic_regression.php" class="topic">Logistic Regression </a>
+                <a href="Decision_tree.php" class="topic">Decision Tree </a>
+                <a href="Regression_vs_classification.php" class="topic">Regression vs. classification</a>
+            </div>
+        </div>
+        <div class="middle_body">
+       <h1> Learning Path </h1>
+<p>This learning path is designed for anyone interested in quickly getting up to speed with machine learning. This learning path consists of step-by-step tutorials with hands-on demonstrations where you will build models and use them in apps.</p>       
+<h2>Understand underlying models</h2>
+<p>once you have mastered the machine learning project, you should learn the working of the models such as Linear Regression, Logistic Regression, Decision Trees, Support vector machines and ensemble learning
+
+In linear regression, you will learn how to draw a straight line through the data points such that the line can be used to predict the outcome. Please note that when there is only one characteristic or feature in data, we can draw the line. If there are many features in the example dataset, we draw a multidimensional plane called a hyperplane.
+
+The logistic regression is basically classification (covered in Classification Topic at CloudxLab) i.e. separating one object from another. In logistic regression, we basically learn to separate the instances using a straight line or plane. You should complement your learning with more projects such as (Noise removal from images, Predicting Titanic Passenger Survival, Building Spam Classifier).
+
+You will also learn how to train the models. This is covered in Training Models at CloudxLab..</p>
+<h2> Unsupervised Learning and Dimensionality Reduction</h2>
+<p>The area of machine learning which deals with problems where the labels are not provided is called Unsupervised Learning. This is fairly complex and is a continuously improving are.
+
+In dimensionality reduction, we learn about how can we reduce the dimensions i.e the features of our data. This helps in reducing the complexity and also overfitting. This is covered in Dimensionality Reduction topic.</p>
+
+
+</ul>
+
+<br><br>
+
+
+
+
+</div>
+        <div class="right_body">
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer_wrapper">
+            <div class="left_footer">
+                <h5 class="footer_h5">AI Learners</h5>
+                <a href="#"><img src="\E-Learning-Website\images\facebook_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\twitter_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\youtube_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\instagram_icon.png" class="footer_icon"></a>
+
+
+
+
+            </div>
+            <div class="middle_footer middle_footer1">
+                <p class="footer_p">Support</p>
+                <a href="#" class="footer_link">Contact Us</a>
+                <a href="#" class="footer_link">FAQ</a>
+                <a href="#" class="footer_link">Lecture</a>
+
+
+            </div>
+            <div class="right_footer">
+                <p class="footer_p">AI Learners</p>
+                <a href="#" class="footer_link">About Us</a>
+                <a href="#" class="footer_link">Careers</a>
+                <a href="#" class="footer_link">Access</a>
+            </div>
+            <div class="sign_up_footer">
+                <span class="footer_h4">Stay up to date on the latest from AI Learners</span>
+                <input type="email" placeholder="Enter  your email" class="input_email_for_sign_up">
+                <input type="button" value="Sign Up" class="sign_up_btn">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        
+        <small class="footer__copyright">© 2021 Copyright</small>
+    </div>
+    </div>
+    </body>
+
+</html>
