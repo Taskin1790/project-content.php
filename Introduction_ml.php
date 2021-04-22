601d64c21a00d52deb4bb360541b79614b3a6526
Taskin1790 <82331915+Taskin1790@users.noreply.github.com>
2021-04-22 12:00:03 +0600

    Add files via upload

---
+++ Introduction_ml.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="\E-Learning-Website\home.css" rel="stylesheet" ">
+    <link href="deep_Learning.css" rel="stylesheet" ">
+   <title>Machine Learning</title>
+</head>
+<body>
+    <div class=" Main_wrapper">
+    <div class="nav_bar">
+        <div class="logo">
+            <img src="\E-Learning-Website\images\innovation_icon1.png" class="nav_bar_icon">
+            <h5 class="nav_bar_logo_name">AI LEARNERS</h5>
+        </div>
+        <div class="menu_name">
+            <ul class="ul_nav">
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\homepage.php">Home</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\artificial_intellegence\introduction_ai.php">Artificial Intellegence</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="Introduction_ml.php">Machine Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Deep_learning\introduction_dl.php">Deep Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Robotics\introduction_robotic.php">Robotics</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\python\introduction_py.php">Python</a></li>
+                <li class="item signUp"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_up\sign_up.php">Sign Up</a></li>
+                <li class="item signIn"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_in\sign_in.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main_body">
+
+        <div class="left_body">
+            <div class="left_body_wrapper">
+
+                <h6 class="topic_header">Deep Learning Topics</h6>
+                <a href="Introduction_ml.php" class="topic">Introduction</a>
+                <a href="Learning_path_ml.php" class="topic">Learning Path</a>
+                <a href="Data_preprocessing.php" class="topic">Data Preprocessing</a>
+                <a href="Features_scaling.php" class="topic">Features Scaling</a>
+                <a href="Gradient_descent.php" class="topic">Gradient Descent</a>
+                <a href="Linear_regression.php" class="topic">Linear Regression </a>
+                <a href="Logistic_regression.php" class="topic">Logistic Regression </a>
+                <a href="Decision_tree.php" class="topic">Decision Tree </a>
+                <a href="Regression_vs_classification.php" class="topic">Regression vs. classification</a>
+            </div>
+        </div>
+        <div class="middle_body">
+       <h1> Welcome to Machine Learning   </h1>
+<p>Machine learning is a method of data analysis that automates analytical model building. It is a branch of   artificial intelligence based on the idea that systems can learn from data, identify patterns and make decisions with minimal human intervention.</p>       
+<h2>Evolution of machine learning</h2>
+<p>Because of new computing technologies, machine learning today is not like machine learning of the past. It was born from pattern recognition and the theory that computers can learn without being programmed to perform specific tasks; researchers interested in artificial intelligence wanted to see if computers could learn from data. The iterative aspect of machine learning is important because as models are exposed to new data, they are able to independently adapt. They learn from previous computations to produce reliable, repeatable decisions and results. It’s a science that’s not new – but one that has gained fresh momentum.</p>
+<p>While many machine learning algorithms have been around for a long time, the ability to automatically apply complex mathematical calculations to big data – over and over, faster and faster – is a recent development. Here are a few widely publicized examples of machine learning applications you may be familiar with:</p>
+
+<ul> 
+    <li>The heavily hyped, self-driving Google car? The essence of machine learning.</li> 
+    <li>Online recommendation offers such as those from Amazon and Netflix? Machine learning applications for everyday life.</li>
+    <li>Knowing what customers are saying about you on Twitter? Machine learning combined with linguistic rule creation.</li>
+    <li>Fraud detection? One of the more obvious, important uses in our world today.</li>
+</ul>
+  <h2>Why is machine learning important?</h2>
+   <p>Resurging interest in machine learning is due to the same factors that have made data mining and Bayesian analysis more popular than ever. Things like growing volumes and varieties of available data, computational processing that is cheaper and more powerful, and affordable data storage.</p>
+<p>All of these things mean it's possible to quickly and automatically produce models that can analyze bigger, more complex data and deliver faster, more accurate results – even on a very large scale. And by building precise models, an organization has a better chance of identifying profitable opportunities – or avoiding unknown risks.</p>
+
+
+
+   <h2>What's required to create good machine learning systems</h2>
+   <ul> 
+    <li>Data preparation capabilities.</li> 
+    <li>Algorithms – basic and advanced.</li>
+    <li>Automation and iterative processes.</li>
+    <li>Scalability.</li>
+    <li>Ensemble modeling.</li>
+</ul>
+
+<br><br>
+
+
+
+
+</div>
+        <div class="right_body">
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer_wrapper">
+            <div class="left_footer">
+                <h5 class="footer_h5">AI Learners</h5>
+                <a href="#"><img src="\E-Learning-Website\images\facebook_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\twitter_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\youtube_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\instagram_icon.png" class="footer_icon"></a>
+
+
+
+
+            </div>
+            <div class="middle_footer middle_footer1">
+                <p class="footer_p">Support</p>
+                <a href="#" class="footer_link">Contact Us</a>
+                <a href="#" class="footer_link">FAQ</a>
+                <a href="#" class="footer_link">Lecture</a>
+
+
+            </div>
+            <div class="right_footer">
+                <p class="footer_p">AI Learners</p>
+                <a href="#" class="footer_link">About Us</a>
+                <a href="#" class="footer_link">Careers</a>
+                <a href="#" class="footer_link">Access</a>
+            </div>
+            <div class="sign_up_footer">
+                <span class="footer_h4">Stay up to date on the latest from AI Learners</span>
+                <input type="email" placeholder="Enter  your email" class="input_email_for_sign_up">
+                <input type="button" value="Sign Up" class="sign_up_btn">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        
+        <small class="footer__copyright">© 2021 Copyright</small>
+    </div>
+    </div>
+    </body>
+
+</html>
