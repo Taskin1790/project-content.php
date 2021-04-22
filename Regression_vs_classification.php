49f2de5874d09382a371336157feb41efaad171f
Taskin1790 <82331915+Taskin1790@users.noreply.github.com>
2021-04-22 12:01:37 +0600

    Add files via upload

---
+++ Regression_vs_classification.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="\E-Learning-Website\home.css" rel="stylesheet" ">
+    <link href="deep_Learning.css" rel="stylesheet" ">
+   <title>Machine Learning</title>
+</head>
+<body>
+    <div class=" Main_wrapper">
+    <div class="nav_bar">
+        <div class="logo">
+            <img src="\E-Learning-Website\images\innovation_icon1.png" class="nav_bar_icon">
+            <h5 class="nav_bar_logo_name">AI LEARNERS</h5>
+        </div>
+        <div class="menu_name">
+            <ul class="ul_nav">
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\homepage.php">Home</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\artificial_intellegence\introduction_ai.php">Artificial Intellegence</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="Introduction_ml.php">Machine Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Deep_learning\introduction_dl.php">Deep Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Robotics\introduction_robotic.php">Robotics</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\python\introduction_py.php">Python</a></li>
+                <li class="item signUp"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_up\sign_up.php">Sign Up</a></li>
+                <li class="item signIn"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_in\sign_in.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main_body">
+
+        <div class="left_body">
+            <div class="left_body_wrapper">
+
+                <h6 class="topic_header">Deep Learning Topics</h6>
+                <a href="Introduction_ml.php" class="topic">Introduction</a>
+                <a href="Learning_path_ml.php" class="topic">Learning Path</a>
+                <a href="Data_preprocessing.php" class="topic">Data Preprocessing</a>
+                <a href="Features_scaling.php" class="topic">Features Scaling</a>
+                <a href="Gradient_descent.php" class="topic">Gradient Descent</a>
+                <a href="Linear_regression.php" class="topic">Linear Regression </a>
+                <a href="Logistic_regression.php" class="topic">Logistic Regression </a>
+                <a href="Decision_tree.php" class="topic">Decision Tree </a>
+                <a href="Regression_vs_classification.php" class="topic">Regression vs. classification</a>
+            </div>
+        </div>
+        <div class="middle_body">
+       <h1> Regression vs. Classification in Machine Learning</h1> 
+       <p>Regression and Classification algorithms are Supervised Learning algorithms. Both the algorithms are used for prediction in Machine learning and work with the labeled datasets. But the difference between both is how they are used for different machine learning problems.
+
+The main difference between Regression and Classification algorithms that Regression algorithms are used to predict the continuous values such as price, salary, age, etc. and Classification algorithms are used to predict/Classify the discrete values such as Male or Female, True or False, Spam or Not Spam, etc</p>      
+
+<h2>Classification</h2>
+<p>Classification is a process of finding a function which helps in dividing the dataset into classes based on different parameters. In Classification, a computer program is trained on the training dataset and based on that training, it categorizes the data into different classes.</p>
+<h2>Types of ML Classification Algorithms:</h2>
+
+<ul> 
+<li>Logistic Regression</li>
+<li>K-Nearest Neighbours</li>
+<li>Support Vector Machines</li>
+<li>Kernel SVM</li>
+<li>Naïve Bayes</li>
+<li>Decision Tree Classification</li>
+<li>Random Forest Classification</li>
+</ul>
+  <h2>Regression:</h2>
+   <p>Regression is a process of finding the correlations between dependent and independent variables. It helps in predicting the continuous variables such as prediction of Market Trends, prediction of House prices, etc.
+
+The task of the Regression algorithm is to find the mapping function to map the input variable(x) to the continuous output variable(y).</p>
+<h2>Types of Regression Algorithm:</h2>
+
+   <ul> 
+   <li>Simple Linear Regression</li>
+   <li>Multiple Linear Regression</li>
+   <li>Polynomial Regression</li>
+   <li>Support Vector Regression</li>
+   <li>Decision Tree Regression</li>
+   <li>Random Forest Regression</li>
+</ul>
+
+<br><br>
+
+
+
+
+</div>
+        <div class="right_body">
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer_wrapper">
+            <div class="left_footer">
+                <h5 class="footer_h5">AI Learners</h5>
+                <a href="#"><img src="\E-Learning-Website\images\facebook_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\twitter_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\youtube_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\instagram_icon.png" class="footer_icon"></a>
+
+
+
+
+            </div>
+            <div class="middle_footer middle_footer1">
+                <p class="footer_p">Support</p>
+                <a href="#" class="footer_link">Contact Us</a>
+                <a href="#" class="footer_link">FAQ</a>
+                <a href="#" class="footer_link">Lecture</a>
+
+
+            </div>
+            <div class="right_footer">
+                <p class="footer_p">AI Learners</p>
+                <a href="#" class="footer_link">About Us</a>
+                <a href="#" class="footer_link">Careers</a>
+                <a href="#" class="footer_link">Access</a>
+            </div>
+            <div class="sign_up_footer">
+                <span class="footer_h4">Stay up to date on the latest from AI Learners</span>
+                <input type="email" placeholder="Enter  your email" class="input_email_for_sign_up">
+                <input type="button" value="Sign Up" class="sign_up_btn">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        
+        <small class="footer__copyright">© 2021 Copyright</small>
+    </div>
+    </div>
+    </body>
+
+</html>
