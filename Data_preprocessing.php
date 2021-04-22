04b5e48615d86ccebe9ef7bdf654c1a937d2729a
Taskin1790 <82331915+Taskin1790@users.noreply.github.com>
2021-04-22 11:57:44 +0600

    Add files via upload

---
+++ Data_preprocessing.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="\E-Learning-Website\home.css" rel="stylesheet" ">
+    <link href="deep_Learning.css" rel="stylesheet" ">
+   <title>Machine Learning</title>
+</head>
+<body>
+    <div class=" Main_wrapper">
+    <div class="nav_bar">
+        <div class="logo">
+            <img src="\E-Learning-Website\images\innovation_icon1.png" class="nav_bar_icon">
+            <h5 class="nav_bar_logo_name">AI LEARNERS</h5>
+        </div>
+        <div class="menu_name">
+            <ul class="ul_nav">
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\homepage.php">Home</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\artificial_intellegence\introduction_ai.php">Artificial Intellegence</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="Introduction_ml.php">Machine Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Deep_learning\introduction_dl.php">Deep Learning</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\Robotics\introduction_robotic.php">Robotics</a></li>
+                <li class="item"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\python\introduction_py.php">Python</a></li>
+                <li class="item signUp"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_up\sign_up.php">Sign Up</a></li>
+                <li class="item signIn"><a class="link active" aria-current="page" href="\E-Learning-Website\topic\sign_in\sign_in.php">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main_body">
+
+        <div class="left_body">
+            <div class="left_body_wrapper">
+
+                <h6 class="topic_header">Deep Learning Topics</h6>
+                <a href="Introduction_ml.php" class="topic">Introduction</a>
+                <a href="Learning_path_ml.php" class="topic">Learning Path</a>
+                <a href="Data_preprocessing.php" class="topic">Data Preprocessing</a>
+                <a href="Features_scaling.php" class="topic">Features Scaling</a>
+                <a href="Gradient_descent.php" class="topic">Gradient Descent</a>
+                <a href="Linear_regression.php" class="topic">Linear Regression </a>
+                <a href="Logistic_regression.php" class="topic">Logistic Regression </a>
+                <a href="Decision_tree.php" class="topic">Decision Tree </a>
+                <a href="Regression_vs_classification.php" class="topic">Regression vs. classification</a>
+            </div>
+        </div>
+        <div class="middle_body">
+       <h1> Data Preprocessing </h1>
+<p>Data preprocessing is a process of preparing the raw data and making it suitable for a machine learning model. It is the first and crucial step while creating a machine learning model.
+
+When creating a machine learning project, it is not always a case that we come across the clean and formatted data. And while doing any operation with data, it is mandatory to clean it and put in a formatted way. So for this, we use data preprocessing task.</p>       
+<h2>Why do we need Data Preprocessing?</h2>
+<p>A real-world data generally contains noises, missing values, and maybe in an unusable format which cannot be directly used for machine learning models. Data preprocessing is required tasks for cleaning the data and making it suitable for a machine learning model which also increases the accuracy and efficiency of a machine learning model.</p>
+<p>It involves below steps:</p>
+
+<ul> 
+    <li>Getting the dataset.</li> 
+    <li>Importing libraries.</li>
+    <li>Importing datasets.</li>
+    <li>Finding Missing Data.</li>
+    <li>Encoding Categorical Data</li>
+    <li>Splitting dataset into training and test set</li>
+    <li>Feature scaling</li>
+</ul>
+<h2> Get the Dataset</h2>
+
+<p>To create a machine learning model, the first thing we required is a dataset as a machine learning model completely works on data. The collected data for a particular problem in a proper format is known as the dataset.
+
+Dataset may be of different formats for different purposes, such as, if we want to create a machine learning model for business purpose, then dataset will be different with the dataset required for a liver patient. So each dataset is different from another dataset. To use the dataset in our code, we usually put it into a CSV file. However, sometimes, we may also need to use an HTML or xlsx file</p>
+</ul>
+
+<br><br>
+
+
+
+
+</div>
+        <div class="right_body">
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+            <p>this is right side</p>
+
+
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer_wrapper">
+            <div class="left_footer">
+                <h5 class="footer_h5">AI Learners</h5>
+                <a href="#"><img src="\E-Learning-Website\images\facebook_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\twitter_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\youtube_icon.png" class="footer_icon"></a>
+                <a href="#"><img src="\E-Learning-Website\images\instagram_icon.png" class="footer_icon"></a>
+
+
+
+
+            </div>
+            <div class="middle_footer middle_footer1">
+                <p class="footer_p">Support</p>
+                <a href="#" class="footer_link">Contact Us</a>
+                <a href="#" class="footer_link">FAQ</a>
+                <a href="#" class="footer_link">Lecture</a>
+
+
+            </div>
+            <div class="right_footer">
+                <p class="footer_p">AI Learners</p>
+                <a href="#" class="footer_link">About Us</a>
+                <a href="#" class="footer_link">Careers</a>
+                <a href="#" class="footer_link">Access</a>
+            </div>
+            <div class="sign_up_footer">
+                <span class="footer_h4">Stay up to date on the latest from AI Learners</span>
+                <input type="email" placeholder="Enter  your email" class="input_email_for_sign_up">
+                <input type="button" value="Sign Up" class="sign_up_btn">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        
+        <small class="footer__copyright">© 2021 Copyright</small>
+    </div>
+    </div>
+    </body>
+
+</html>
